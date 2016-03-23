3ecb6fa92bc2a53ecd81e0c45ad880b2dba25483
queri <querimoni@gmail.com>
2016-03-23 17:44:43 +0300

    Поправлены стили и доделан js

--- js/script.js
+++ js/script.js
@@ -67,4 +67,293 @@ var toggler = document.getElementById('toggler');
     document.getElementById('nav').classList.toggle('mobile-nav-visible');
   }
 
+
+  var owl = $('#carousel-hall');
+  owl.owlCarousel({
+    'items':8,
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+        'mergeFit':true
+      },
+      400:{
+        'items': 3,
+        'center': true,
+        'mergeFit':true
+      },
+      500:{
+        'items': 4,
+        'center': true,
+        'mergeFit':true
+      },
+      678:{
+        'items': 5,
+        'center': true,
+        'loop':true,
+        'mergeFit':true
+      },
+      900:{
+        'center': false,
+        'mouseDrag': true,
+      },
+      1100:{
+        'mouseDrag': true,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-kitchen');
+  owl.owlCarousel({
+    'items':'all',
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+        'mergeFit':true
+      },
+      400:{
+        'items': 3,
+        'center': true,
+        'mergeFit':true
+      },
+      500:{
+        'items': 4,
+        'center': true,
+        'mergeFit':true
+      },
+      678:{
+        'items': 5,
+        'center': true,
+        'mergeFit':true
+      },
+      1000:{
+        'mouseDrag': false,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-toilet');
+  owl.owlCarousel({
+    'items':'all',
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+        'mergeFit':true
+      },
+      400:{
+        'items': 3,
+        'center': true,
+        'mergeFit':true
+      },
+      500:{
+        'items': 4,
+        'center': true,
+        'mergeFit':true
+      },
+      678:{
+        'items': 5,
+        'center': false,
+        'mergeFit':true
+      },
+      1000:{
+        'mouseDrag': false,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-12_room');
+  owl.owlCarousel({
+    'items':'all',
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+        'mergeFit':true
+      },
+      400:{
+        'items': 3,
+        'center': false,
+        'mergeFit':true
+      },
+      500:{
+        'items': 4,
+        'center': false,
+        'mergeFit':true
+      },
+      678:{
+        'items': 5,
+        'center': false,
+        'mergeFit':true
+      },
+      1000:{
+        'mouseDrag': false,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-6_room');
+  owl.owlCarousel({
+    'items':'all',
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'itemsScaleUp': true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+        'mergeFit':false
+      },
+      400:{
+        'items': 3,
+        'center': true,
+        'mergeFit':false
+      },
+      500:{
+        'items': 4,
+        'loop':true,
+        'center': false,
+        'mergeFit':false
+      },
+      678:{
+        'items': 9,
+        'center': false,
+        'loop':false,
+        'mergeFit':false
+      },
+      990:{
+        'center': false,
+        'mouseDrag': false,
+      },
+      1100:{
+        'mouseDrag': false,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-2_room');
+  owl.owlCarousel({
+    'items':'all',
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+      },
+      400:{
+        'items': 3,
+        'center': true,
+      },
+      500:{
+        'items': 4,
+        'center': false,
+      },
+      678:{
+        'items': 7,
+        'center': false,
+        'mergeFit':true
+      },
+      990:{
+        'center': false,
+        'mouseDrag': false,
+      },
+      1100:{
+        'mouseDrag': false,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-lux');
+  owl.owlCarousel({
+    'items':'all',
+    'loop':true,
+    'margin':5,
+    'merge':true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+      },
+      400:{
+        'items': 3,
+        'center': true,
+      },
+      500:{
+        'items': 4,
+        'center': true,
+      },
+      678:{
+        'items': 10,
+        'loop':true,
+        'mergeFit':true
+      },
+      990:{
+        'center': false,
+        'mouseDrag': false,
+      },
+      1100:{
+        'mouseDrag': false,
+        'mergeFit':false
+      }
+    }
+  });
+
+  var owl = $('#carousel-roof');
+  owl.owlCarousel({
+    'items':'all',
+    'margin':5,
+    'loop': true,
+    'merge': true,
+    'responsive':{
+      320:{
+        'items': 2,
+        'center': true,
+      },
+      400:{
+        'items': 3,
+        'center': true,
+      },
+      500:{
+        'items': 4,
+        'center': false,
+      },
+      678:{
+        'items': 5,
+        'center': false,
+        'mergeFit':true
+      },
+      990:{
+        'center': false,
+        'mouseDrag': false,
+      },
+      1100:{
+        'mergeFit':false,
+        'mouseDrag': false,
+      }
+    }
+  });
+
+
 });
